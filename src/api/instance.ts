08c9e7fae50de1aae25e8d4f5a0b53d7e414c6a4
kimjinwoo <rlawlsdn177@naver.com>
2024-07-29 15:54:20 +0900

    feat: api 인스턴스 생성

---
+++ src/api/instance.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:3000',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        console.log('Request started:', config);
+        return config;
+    },
+    (error) => {
+        console.error('Request error:', error);
+        return Promise.reject(error);
+    }
+);
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        console.log('Response received:', response);
+        return response;
+    },
+    (error) => {
+        console.error('Response error:', error);
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
